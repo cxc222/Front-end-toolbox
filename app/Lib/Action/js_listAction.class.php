0ee7d32ba8c85ca28ac769db6272ea257aae3291
cxc222 <359170248@qq.com>
2013-01-29 17:57:59 +0800

    只实现了利用 YUI Compressor 压缩js~~ 比较低效率~~~ Signed-off-by: cxc222 <359170248@qq.com>

--- app/Lib/Action/js_listAction.class.php
+++ app/Lib/Action/js_listAction.class.php
@@ -15,21 +15,42 @@ class js_listAction extends baseAction {
     //压缩js 或者 css代码
     public function jscompress(){
     	if ($this->isPost()) {
-    		import("@.Action.yui-compressor");
-    		$yui = new yuicompressor(array(
-    				'java_home'=>'C:\Program Files\Java\jdk1.7.0_09\bin\java.exe', //或自己指定 jdk 安装的 bin 目录 (绝对路径)
-    				'jar_file'=>'D:\xampp\htdocs\Front-end-toolbox\jar\yuicompressor-2.4.7.jar',
-    				'save_path'=>'D:\xampp\htdocs\Front-end-toolbox\temp', //必须有可写权限
-    				// -------- 全局设置 --------- //
-    				'charset'=>'utf-8', //文件的编码
-    				'line-break'=>false, //在指定的列后插入一个 line-bread 符号
-    				// -------- javascript 代码的配置选项 --------- //
-    				'nomunge'=>false,  //只是简单压缩，清除空行空格注释等。
-    				'semi'=>false, //保留所有的分号
-    				'optimizations'=>false, //禁止优化代码.
-    		));
+    		if($_POST['js']){
+    			$data = array('status'=>0, 'data'=>null, 'info'=> null);
+    			ini_set('memory_limit','-1');
+    			$filename = time();
+    			$path = ROOT_PATH."/temp/js/".$filename.".js";
+    			$fp = fopen($path,"w+");
+    			$content = $_POST['js'];
+    			fwrite($fp,$content);
+    			fclose($fp);
+    			
+    			import("@.Action.yui-compressor");
+    			$yui = new yuicompressor(array(
+    					'java_home'=>ROOT_PATH.'/jar/jdk1.7.0_09/bin/java', //或自己指定 jdk 安装的 bin 目录 (绝对路径)
+    					'jar_file' =>ROOT_PATH.'/jar/yuicompressor-2.4.7.jar',
+    					'save_path'=> ROOT_PATH."/temp/js/", //必须有可写权限
+    					// -------- 全局设置 --------- //
+    					'charset'=>'utf-8', //文件的编码
+    					'line-break'=>false, //在指定的列后插入一个 line-bread 符号
+    					// -------- javascript 代码的配置选项 --------- //
+    					'nomunge'=>false,  //只是简单压缩，清除空行空格注释等。
+    					'semi'=>false, //保留所有的分号
+    					'optimizations'=>false, //禁止优化代码.
+    			));
+    			$resutlt = $yui->compress($path);
+    			if($resutlt['status'] == 0 && $resutlt['success']){
+    				$file = file_get_contents($resutlt['success']);
+    				$file = strip_tags($file);
+    				$data['status'] = 1;
+    				$data['data'] = $file;
+    			}
+    			@unlink($path);
+    			@unlink($resutlt['success']);
+    			$this->ajaxReturn($data);
+    		}
+    		
     	}
     	$this->display();
     }
-    
 }
