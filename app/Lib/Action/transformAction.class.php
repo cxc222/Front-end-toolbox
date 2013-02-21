a7140f608fc5864ac1ec1e1d62cf0292608ebb31
cxc222 <359170248@qq.com>
2013-02-21 11:02:28 +0800

    增加 二维码生成!!!!

--- app/Lib/Action/transformAction.class.php
+++ app/Lib/Action/transformAction.class.php
@@ -7,7 +7,7 @@
 class transformAction extends baseAction {
 	
 	public function _initialize(){
-		$title_name = '<div class="toolName">在线生成二维码(QR码)-采用<a href="http://www.oschina.net/p/zxing" id="zxing" data-original-title="">ZXing</a>与<a href="http://www.d-project.com/">d-project</a><a style="float:right;text-decoration:none;" href="#advice" data-toggle="modal"><span class="badge badge-important"><i class="icon-envelope icon-white"></i> Feedback</span></a></div>';
+		$title_name = '<div class="toolName">在线生成二维码(QR码)-采用<a href="http://www.oschina.net/p/zxing" id="zxing" data-original-title="">phpqrcode</a><a style="float:right;text-decoration:none;" href="#advice" data-toggle="modal"><span class="badge badge-important"><i class="icon-envelope icon-white"></i> Feedback</span></a></div>';
 		//定义动作
 		$action = array(
 				'generate_qrcode' => L('generate_qrcode')
@@ -30,40 +30,38 @@ class transformAction extends baseAction {
 		
 		$content = 'http://tool.qdzff.com/';
 		
-		if($this->isPost()){
-			if($_POST['output'] && $_POST['error'] && $_POST['margin'] && $_POST['size'] && $_POST['data']){
-				//纠错等级
-				switch ($_POST['error']){
-					case 'L':
-						$level = 'QR_ECLEVEL_L';
-						break;
-					case 'M':
-						$level = 'QR_ECLEVEL_M';
-						break;
-					case 'Q':
-						$level = 'QR_ECLEVEL_Q';
-						break;
-					case 'H':
-						$level = 'QR_ECLEVEL_H';
-						break;
-					default:
-						$level = 'QR_ECLEVEL_L';
-						break;
-				}
-				
-				//大小
-				if($_POST['size'] == 0){
-					$size = '3';
-				}else{
-					$size = $_POST['size'];
-				}
-				
-				//margin 留边
-				$margin = $_POST['margin'];
-				
-				//内容
-				$content = $_POST['data'];
+		if($_GET['output'] && $_GET['error'] && $_GET['size'] && $_GET['data']){
+			//纠错等级
+			switch ($_GET['error']){
+				case 'L':
+					$level = 0;
+					break;
+				case 'M':
+					$level = 1;
+					break;
+				case 'Q':
+					$level = 2;
+					break;
+				case 'H':
+					$level = 3;
+					break;
+				default:
+					$level = 0;
+					break;
 			}
+			
+			//大小
+			if($_GET['size'] == 0){
+				$size = '3';
+			}else{
+				$size = $_GET['size'];
+			}
+			
+			//margin 留边
+			$margin = $_GET['margin'];
+			
+			//内容
+			$content = $_GET['data'];
 		}
 		QRcode::png($content,false,$level,$size,$margin);
 		die();
